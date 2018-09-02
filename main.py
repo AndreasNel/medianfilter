9430e4aa943a18ded153223ccefeb289f3e93905
AndreasNel <nel.andreas1@gmail.com>
2018-09-02 22:34:14 +0200

    definition of structural element and gray levels

--- main.py
+++ main.py
@@ -7,12 +7,51 @@ import math
 IMAGE_DIR = "images"
 KERNEL_SIZE = 3
 BUFFER_SIZE = math.floor(KERNEL_SIZE/2)
+TEMPLATE = [
+    [0, 1, 0],
+    [1, 1, 1],
+    [0, 1, 0],
+]
+
+
+def minkowski_erosion(image):
+    maxw = image.width
+    maxh = image.height
+    result = image.copy()
+    gray_levels = [-1 for _ in TEMPLATE[0]]
+    for idx, i in enumerate(TEMPLATE):
+        for j in range(len(i)):
+            gray_levels[j] += TEMPLATE[idx][j]
+    print(gray_levels)
+    for x in range(len(TEMPLATE[0]), maxw - len(TEMPLATE[0])):
+        for y in range(len(TEMPLATE[0]), maxh - len(TEMPLATE)):
+            box = (x - len(TEMPLATE[0]), y - len(TEMPLATE), x + len(TEMPLATE[0]) + 1, y + len(TEMPLATE) + 1)
+            cropped = image.crop(box)
+            # TODO finish this
+    return result
+
+
+def minkowski_dilation(image):
+    maxw = image.width
+    maxh = image.height
+    result = image.copy()
+    gray_levels = [-1 for _ in TEMPLATE[0]]
+    for idx, i in enumerate(TEMPLATE):
+        for j in range(len(i)):
+            gray_levels[j] += TEMPLATE[idx][j]
+    print(gray_levels)
+    for x in range(len(TEMPLATE[0]), maxw - len(TEMPLATE[0])):
+        for y in range(len(TEMPLATE[0]), maxh - len(TEMPLATE)):
+            box = (x - len(TEMPLATE[0]), y - len(TEMPLATE), x + len(TEMPLATE[0]) + 1, y + len(TEMPLATE) + 1)
+            cropped = image.crop(box)
+            # TODO finish this
+    return result
 
 
 def median_filter(image):
     # The following is only done in order to compare my method with the built-in methods.
     filtered = image.filter(ImageFilter.MedianFilter(KERNEL_SIZE))
-    filtered.convert('RGB').save("median_{}".format(filename))
+    # filtered.convert('RGB').save("median_{}".format(filename))
 
     # This is the start of the manual implementation
     maxw = image.width
@@ -38,7 +77,7 @@ def median_filter(image):
     print('Mine: ', end='')
     print([result.getpixel((BUFFER_SIZE, BUFFER_SIZE)), result.getpixel((255 - BUFFER_SIZE, BUFFER_SIZE)),
            result.getpixel((BUFFER_SIZE, 255 - BUFFER_SIZE)), result.getpixel((255 - BUFFER_SIZE, 255 - BUFFER_SIZE))])
-    result.convert('RGB').save('results_{}'.format(filename))
+    return result
 
 
 if __name__ == '__main__':
@@ -47,5 +86,9 @@ if __name__ == '__main__':
         filename = os.fsdecode(file)
         print("READING {}".format(filename))
         image = Image.open("{}/{}".format(IMAGE_DIR, filename)).convert('L')
-        median_filter(image)
-
+        result = median_filter(image)
+        result.convert('RGB').save('results_{}'.format(filename))
+        eroded = minkowski_erosion(result)
+        eroded.convert('RGB').save('eroded_{}'.format(filename))
+        dilated = minkowski_dilation(result)
+        dilated.convert('RGB').save('dilated_{}'.format(filename))
