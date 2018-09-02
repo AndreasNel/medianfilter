4589562ef028659c1fd0b0b8574f469a9729e05f
AndreasNel <nel.andreas1@gmail.com>
2018-09-02 21:33:13 +0200

    modified to easily change kernel size

--- main.py
+++ main.py
@@ -1,9 +1,45 @@
 from PIL import Image, ImageFilter
 import os
 import statistics
+import math
 
 
 IMAGE_DIR = "images"
+KERNEL_SIZE = 3
+BUFFER_SIZE = math.floor(KERNEL_SIZE/2)
+
+
+def median_filter(image):
+    # The following is only done in order to compare my method with the built-in methods.
+    filtered = image.filter(ImageFilter.MedianFilter(KERNEL_SIZE))
+    filtered.convert('RGB').save("median_{}".format(filename))
+
+    # This is the start of the manual implementation
+    maxw = image.width
+    maxh = image.height
+    result = image.copy()
+    for x in range(BUFFER_SIZE, maxw - BUFFER_SIZE):
+        for y in range(BUFFER_SIZE, maxh - BUFFER_SIZE):
+            # Define the box used to get the pixels from the image.
+            box = (x - BUFFER_SIZE, y - BUFFER_SIZE, x + BUFFER_SIZE + 1, y + BUFFER_SIZE + 1)
+            # Get the pixels.
+            cropped = image.crop(box)
+            # Get the median.
+            medianvalue = int(statistics.median(cropped.getdata()))
+            # Write the resulting pixels to the resulting image.
+            result.putpixel((x, y), medianvalue)
+    print('Original image: ', end='')
+    print([image.getpixel((BUFFER_SIZE, BUFFER_SIZE)), image.getpixel((255 - BUFFER_SIZE, BUFFER_SIZE)),
+           image.getpixel((BUFFER_SIZE, 255 - BUFFER_SIZE)), image.getpixel((255 - BUFFER_SIZE, 255 - BUFFER_SIZE))])
+    print('Built-in function: ', end='')
+    print([filtered.getpixel((BUFFER_SIZE, BUFFER_SIZE)), filtered.getpixel((255 - BUFFER_SIZE, BUFFER_SIZE)),
+           filtered.getpixel((BUFFER_SIZE, 255 - BUFFER_SIZE)),
+           filtered.getpixel((255 - BUFFER_SIZE, 255 - BUFFER_SIZE))])
+    print('Mine: ', end='')
+    print([result.getpixel((BUFFER_SIZE, BUFFER_SIZE)), result.getpixel((255 - BUFFER_SIZE, BUFFER_SIZE)),
+           result.getpixel((BUFFER_SIZE, 255 - BUFFER_SIZE)), result.getpixel((255 - BUFFER_SIZE, 255 - BUFFER_SIZE))])
+    result.convert('RGB').save('results_{}'.format(filename))
+
 
 if __name__ == '__main__':
     directory = os.fsencode(IMAGE_DIR)
@@ -11,28 +47,5 @@ if __name__ == '__main__':
         filename = os.fsdecode(file)
         print("READING {}".format(filename))
         image = Image.open("{}/{}".format(IMAGE_DIR, filename)).convert('L')
+        median_filter(image)
 
-        # The following is only done in order to compare my method with the built-in methods.
-        filtered = image.filter(ImageFilter.MedianFilter(3))
-        filtered.convert('RGB').save("median_{}".format(filename))
-
-        maxw = image.width
-        maxh = image.height
-        result = image.copy()
-        for x in range(1, maxw - 1):
-            for y in range(1, maxh - 1):
-                # Define the box used to get the pixels from the image.
-                box = (x - 1, y - 1, x + 2, y + 2)
-                # Get the pixels.
-                cropped = image.crop(box)
-                # Get the median.
-                medianvalue = int(statistics.median(cropped.getdata()))
-                # Write the resulting pixels to the resulting image.
-                result.putpixel((x,y), medianvalue)
-        print('Original image: ', end='')
-        print([image.getpixel((1,1)), image.getpixel((254,1)), image.getpixel((1,254)), image.getpixel((254,254))])
-        print('Built-in function: ', end='')
-        print([filtered.getpixel((1,1)), filtered.getpixel((254,1)), filtered.getpixel((1,254)), filtered.getpixel((254,254))])
-        print('Mine: ', end='')
-        print([result.getpixel((1,1)), result.getpixel((254,1)), result.getpixel((1,254)), result.getpixel((254,254))])
-        result.convert('RGB').save('results_{}'.format(filename))
